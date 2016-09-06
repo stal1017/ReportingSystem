8a19f30961ccbd45ab6b5da94c6fae5d30678d3c
stal1017 <guapo_alex@web.de>
2016-09-06 21:26:44 +0200

    Report1_View eingebaut

--- Angular/Controller/Report1_Controller.js
+++ Angular/Controller/Report1_Controller.js
@@ -1,3 +1,15 @@
-/**
- * Created by Alex on 06.09.2016.
- */
+'use strict';
+
+angular.module('myApp.Report1_View', ['ngRoute'])
+
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/Report1', {
+            templateUrl: 'Angular/Views/Report1_View.html',
+            controller: 'Report1_Ctrl'
+        });
+    }])
+
+    .controller('Report1_Ctrl', [function() {
+
+        console.log("Report1_Ctrl geladen");
+    }]);
